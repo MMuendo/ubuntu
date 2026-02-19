36ec6312d77783240821dd0c5ba38a4cce779cbc
Ezra Muinde <98468975+MMuendo@users.noreply.github.com>
2026-02-20 00:08:06 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -63,7 +63,7 @@ const App: React.FC = () => {
                             <AcademyPage />
                         </Layout>
                     }
-                </Route>
+                />
 
 
                 {/* Dynamic Course Route wrapped in Layout */}
