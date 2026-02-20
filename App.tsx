8c4da025e480927a9776d46b71413c1142868e69
Ezra Muinde <98468975+MMuendo@users.noreply.github.com>
2026-02-20 14:41:07 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -77,7 +77,7 @@ const App: React.FC = () => {
                 />
 
                 <Route 
-                    path="/webinar" 
+                    path="/webinar/:id" 
                     element={
                         <Layout>
                             <WebinarDetailsPage />
